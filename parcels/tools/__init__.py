88a463fcd1b7cc52ae48ad5752056d9a02dbaf06
Christian Kehl <c.kehl@uu.nl>
2020-06-15 16:08:40 +0200

    moved some stray compilation functions in the 'global_statics' module (holding globally static functions) and added the ID generators to the framework (as they are global statics too). The statics are one of the uncertainties still to be seen and tested with MPI

--- parcels/tools/__init__.py
+++ parcels/tools/__init__.py
@@ -3,3 +3,8 @@ from .error import *  # noqa
 from .interpolation_utils import *  # noqa
 from .loggers import *  # noqa
 from .timer import *  # noqa
+from .global_statics import *  # noga
+from .id_generators import *  # noga
+
+global idgen
+idgen = SpatioTemporalIdGenerator()
