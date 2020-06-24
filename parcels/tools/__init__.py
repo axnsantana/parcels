d32a3f41e768f08d454ca3dfeb3fd9e2bd36e5f3
Christian Kehl <c.kehl@uu.nl>
2020-06-24 22:28:34 +0200

    sorted_pset_trials - ID generation working again for all but many-MPI configs - getting a segmentation fault, so I assume the ID linking doesn't work in that configuration. Requires further investigation.

--- parcels/tools/__init__.py
+++ parcels/tools/__init__.py
@@ -6,7 +6,8 @@ from .timer import *  # noqa
 from .global_statics import *  # noga
 from .id_generators import *  # noga
 
-global idgen
-idgen = SpatioTemporalIdGenerator()
-idgen.setDepthLimits(0, 100)
+# global idgen
+# idgen = SpatioTemporalIdGenerator()
+idgen = GenerateID_Service(SpatioTemporalIdGenerator)
+idgen.setDepthLimits(0, 100.0)
 idgen.setTimeLine(0, 240.0)
